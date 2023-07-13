39a31e8d4cf8c5a8b50baaaf010300ce37a67775
David Yuan <yua002@henrico.us>
2023-07-13 14:07:25 -0400

    actions and reducers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
 import './App.css';
 import GridBoard from './components/GridBoard';
 import NextBlock from './components/NextBlock';
+import ScoreBoard from './components/ScoreBoard';
+import Controls from './components/Controls';
+import MessagePopup from './components/MessagePopup';
+
+const store = createStore(reducers)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">DER TETRIS</h1>
-      </header>
-      <GridBoard />
-      <NextBlock />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">DER TETRIS</h1>
+        </header>
+        <GridBoard />
+        <NextBlock />
+        <ScoreBoard />
+        <Controls />
+      </div>
+    </Provider>
   );
 }
 
